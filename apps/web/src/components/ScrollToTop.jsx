03bbfee3b8ed2e682f78372b17c59be54988e25c
arcusvictus-rgb <arcusvictus@gmail.com>
2026-05-08 19:44:24 +0530

    Update ScrollToTop.jsx

--- apps/web/src/components/ScrollToTop.jsx
+++ apps/web/src/components/ScrollToTop.jsx
@@ -1,10 +1,10 @@
 import { useLocation } from 'react-router-dom';
-import { useLayoutEffect } from 'react';
+import { useEffect } from 'react';
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
     }, [pathname]);
 
