7e2134b0c6c21eb10992117222b0fb45e3caeea8
arcusvictus-rgb <arcusvictus@gmail.com>
2026-05-08 18:38:56 +0530

    Create App.jsx

---
+++ apps/web/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Toaster } from '@/components/ui/sonner';
+import ScrollToTop from './components/ScrollToTop.jsx';
+import HomePage from './pages/HomePage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import ServicesPage from './pages/ServicesPage.jsx';
+import TeamPage from './pages/TeamPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  );
+}
+
+export default App;
